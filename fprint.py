c7360f0df77d7e2822f467937b8f2c8e468eb29a
Tfloow <88899420+Tfloow@users.noreply.github.com>
2022-10-19 18:08:17 +0200

    now added 2 letters

--- fprint.py
+++ fprint.py
@@ -1,2 +1,10 @@
-h = "|\t\t|\n|\t\t|\n|_______|\n|\t\t|\n|\t\t|"
-print(h+h)
+h = [["|     |\t"],["|     |\t"], ["|_____|\t"], ["|     |\t"], ["|     |\t"]]
+e = [["       \t"],["      \t"],["  ___ \t"], [" /___\ \t"], [" \___\t"]]
+def add(fromn, n):
+    for f in range(len(n)):
+        fromn[f][0] += n[f][0]
+    return fromn
+e = add(h, e)
+e = add(e, e)
+for f in e:
+    print(f[0])
