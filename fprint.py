3853e8f146eea4058265bfdf060f1b58ee35b383
Tfloow <88899420+Tfloow@users.noreply.github.com>
2022-10-19 19:19:12 +0200

    added the first three letter and the asciiLetter

--- fprint.py
+++ fprint.py
@@ -1,10 +1,21 @@
-h = [["|     |\t"],["|     |\t"], ["|_____|\t"], ["|     |\t"], ["|     |\t"]]
-e = [["       \t"],["      \t"],["  ___ \t"], [" /___\ \t"], [" \___\t"]]
+test = [["       \t"],["      \t"],["  ___ \t"], [" / \t"], [" \___\t"]]
+lowerTemplate = [["       \t"],["      \t"],[""], [""], [""]]
+# maximum 3 lines height
+low = {"a": [["       \t"],["      \t"],["  ___ \t"], ["  ___\ \t"], [" \___/\t"]],"b": [["       \t"],["      \t"],[" |   \t"], [" |___ \t"], [" |___|\t"]],"c": [["       \t"],["      \t"],["  ___ \t"], [" / \t"], [" \___\t"]], "e": [["       \t"],["      \t"],["  ___ \t"], [" /___\ \t"], [" \___\t"]], "h": [["|     |\t"],["|     |\t"], ["|_____|\t"], ["|     |\t"], ["|     |\t"]]}
 def add(fromn, n):
     for f in range(len(n)):
         fromn[f][0] += n[f][0]
     return fromn
-e = add(h, e)
-e = add(e, e)
-for f in e:
+
+word = input("what word ?")
+first = True
+
+for g in word:
+    if first:
+        test = low[g]
+        first = False
+    else:
+        add(test, low[g])
+
+for f in test:
     print(f[0])
